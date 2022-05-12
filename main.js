80d8d2f84748a39a5f83bcf82722c2944dc88a63
Connor <connor.doherty97@gmail.com>
2022-05-11 22:16:51 -0400

    final

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 function bulbMaze(map){
-  //coding and coding..
-  
-  let arr = map.split('')
-  
-  
-  for(let i=0; i<arr.length; i++){
-    if(arr[i] === arr[i+1]){
-      return false
-    }else if(arr[i] === '' && arr[i-1] === arr[i+1]){
-      return false
-    }else{
-      return true
+function bulbMaze(map){
+  let result = 0;
+  if (map[0]!=="o"){   
+    for (let i = 0 ; i < map.length; i++){
+      if (i%2===0){
+        if (map[i]!=="o"){
+          result+=1;
+        } else {
+          result-=1;
+        }
+      } else {
+        if (map[i]!=="x"){
+          result+=1;
+        } else {
+          result-=1;
+        }
+      }
     }
-    
+    if (result === map.length){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false
   }
   
+}
 }
