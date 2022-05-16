3ec0b00fbbf45c63b58f7f130ffe0d7518bba400
Connor <connor.doherty97@gmail.com>
2022-05-16 13:10:33 -0400

    working on some cases that do not pass test

---
+++ goingToTheCinema.js
@@ -0,0 +1,23 @@
+function movie(card, ticket, perc) {
+    // your code
+
+    function movie(card, ticket, perc) {
+        // your code
+      
+      let visits = 0
+      
+      let cardTotal = card
+      
+      let ticketTotal = 0
+      
+      let lastTicket = ticket
+      
+      while(cardTotal > ticketTotal){
+        ticketTotal += ticket
+        lastTicket = lastTicket * perc
+        cardTotal += lastTicket
+        visits+=1
+      }
+      return visits
+    };
+};
