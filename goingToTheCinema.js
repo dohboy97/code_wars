560fdbab140d2f281e08cbbd913d23dbe73fb6af
Connor <connor.doherty97@gmail.com>
2022-05-16 13:17:07 -0400

    solution found, had to use math.ceil

--- goingToTheCinema.js
+++ goingToTheCinema.js
@@ -1,23 +1,21 @@
 function movie(card, ticket, perc) {
     // your code
-
-    function movie(card, ticket, perc) {
-        // your code
-      
-      let visits = 0
-      
-      let cardTotal = card
-      
-      let ticketTotal = 0
-      
-      let lastTicket = ticket
-      
-      while(cardTotal > ticketTotal){
-        ticketTotal += ticket
-        lastTicket = lastTicket * perc
-        cardTotal += lastTicket
-        visits+=1
-      }
-      return visits
-    };
+  
+  let visits = 0
+  
+  let cardTotal = card
+  
+  let ticketTotal = 0
+  
+  let lastTicket = ticket
+  
+  while(Math.ceil(cardTotal) >= ticketTotal){
+    ticketTotal += ticket
+    lastTicket = lastTicket * perc
+    
+      cardTotal += lastTicket
+  
+    visits+=1
+  }
+  return visits
 };
