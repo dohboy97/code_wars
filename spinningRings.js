dbed9e32174873ca25063a4925065dfaa812a703
Connor <connor.doherty97@gmail.com>
2022-05-17 15:20:44 -0400

    pseudo code

---
+++ spinningRings.js
@@ -0,0 +1,23 @@
+function spinningRings(innerMax, outerMax) {
+    // your code here
+    
+    //start at 0 for both rings, innerMax and outerMax their respective limits before resetting to 0 
+    
+    let inner = 0
+    
+    let outer = 0
+    
+    let turns = 0 //tracks total turns
+    
+    while(inner !== outer){
+      //conditional that sets rings to 0 after their max
+      
+      
+      
+      turns++ //keeps track of total turns
+    }
+    
+    
+    
+    
+  };
