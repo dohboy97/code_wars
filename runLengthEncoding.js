6bc0e56fb271ca728fba466a7a4f1b02b5fb873b
Connor <connor.doherty97@gmail.com>
2022-05-18 16:35:14 -0400

    times out

--- runLengthEncoding.js
+++ runLengthEncoding.js
@@ -1,8 +1,25 @@
 var runLengthEncoding = function(str){
   
+    let arr = str.split ('')
+    //turning string into an array to run through a loop
+    
+      let result = []
+    //array for the results to push to
+    
+    for(let i = 0; i< arr.length; i++){
+      let toPush = [1,arr[i]]
+      while(arr[i]===arr[i+1]){
+        toPush[0] = toPush[0]+1
+      }
+      result.push(toPush)
+    }
+    
+    
   
-  
-  
-  
+    
+    
+    return result
+    
+    
     //return 
   }
