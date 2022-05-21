f6b02947134ffd60dc83d113bd597bac8c95b13f
Connor <connor.doherty97@gmail.com>
2022-05-21 15:02:27 -0400

    further testing

--- sumOfParts.js
+++ sumOfParts.js
@@ -9,7 +9,7 @@ function partsSums(ls) {
     
     //loop that adds array elements t/g with reduce and then pushes them to the new array at the end, and then shifts the first element
     
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < startArray.length; i++){
       
       let sum = startArray.reduce((acc,c) => acc+c,)
       0
