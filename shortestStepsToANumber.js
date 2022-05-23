88d22a87e9d6a2c7134e523865309911045976d0
Connor <connor.doherty97@gmail.com>
2022-05-23 16:03:04 -0400

    framework

---
+++ shortestStepsToANumber.js
@@ -0,0 +1,19 @@
+function shortestStepsToNum(num) {
+  
+    //a step can be x+1 or x*2
+    
+    let x = 1;
+    let counter = 0;
+    
+    while(x !== num){
+      
+      //add one or divide by two
+      
+      
+      //iterate counter
+      counter++
+    }
+    
+    
+    return counter
+  }
