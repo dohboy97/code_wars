eb86b70f09184777fd6cd18bfc9d999b4fd29250
Connor <connor.doherty97@gmail.com>
2022-05-23 16:11:30 -0400

    trial run

--- shortestStepsToANumber.js
+++ shortestStepsToANumber.js
@@ -3,11 +3,19 @@ function shortestStepsToNum(num) {
     //a step can be x+1 or x*2
     
     let x = 1;
-    let counter = 0;
+    let counter = 0
     
-    while(x !== num){
+    
+    
+    while(x < num){
       
       //add one or divide by two
+      
+      if (x*2 < num){
+        x*=2
+      }else{
+        x+=1
+      }
       
       
       //iterate counter
