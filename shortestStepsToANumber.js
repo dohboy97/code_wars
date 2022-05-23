02b457a22efc01d9d203af7d7a95f08115443c9a
Connor <connor.doherty97@gmail.com>
2022-05-23 16:39:38 -0400

    actually needed to count back from num to one

--- shortestStepsToANumber.js
+++ shortestStepsToANumber.js
@@ -2,21 +2,21 @@ function shortestStepsToNum(num) {
   
     //a step can be x+1 or x*2
     
-    let x = 1;
+    let x = num;
     let counter = 0
     
     
     
-    while(x < num){
+    while(x > 1){
       
-      //add one or divide by two
+      //actually need to go backwards from num to find quickest solution. div by 2 if modulous === 0, subtract 1 if not
       
-      if (x*2 < num){
-        x*=2
+      if (x%2===0){
+        x/=2
       }else{
-        x+=1
+        x-=1
       }
-      
+  
       
       //iterate counter
       counter++
