1e17d577e2bf7b617a924fbbc09eb5a7bd37e144
Connor <connor.doherty97@gmail.com>
2022-05-24 21:49:04 -0400

    pseudo

---
+++ reachMeAndSumMyDigits.js
@@ -0,0 +1,10 @@
+function sumDigNthTerm(initval, patternl, nthterm) {
+    // your code
+  
+  let val = initval 
+  
+  //create a for loop that runs until the incrementer is equal to the length of nth term
+  
+  //stringify the value at nth term in order to return the sum
+  
+}
