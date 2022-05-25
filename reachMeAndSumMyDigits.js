b6348a17d056a7e1c1dfe0f081d3503a8972bd32
Connor <connor.doherty97@gmail.com>
2022-05-24 22:02:28 -0400

    just need function to stringify and grab sums

--- reachMeAndSumMyDigits.js
+++ reachMeAndSumMyDigits.js
@@ -2,9 +2,24 @@ function sumDigNthTerm(initval, patternl, nthterm) {
     // your code
   
   let val = initval 
+  let indexCounter = 0
+  let termCounter = 1
+  //create a loop that runs until the incrementer is equal to the length of nth term
   
-  //create a for loop that runs until the incrementer is equal to the length of nth term
+  while(termCounter < nthterm){
+    if(indexCounter === patternl.length){
+      indexCounter = 0
+    }
+    
+    val+= patternl[indexCounter]
+    
+    termCounter++
+    indexCounter++
+  }
   
+  //term counter cannot be greater than nthterm, index counter gets reset to 0 once = patternl.length
+  console.log(val)
+
   //stringify the value at nth term in order to return the sum
   
 }
