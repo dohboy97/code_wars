ff464522df57b39bfaf48d3f30454d0bc02cd84f
Connor <connor.doherty97@gmail.com>
2022-05-27 11:33:26 -0400

    final solution

--- digitalCypher.js
+++ digitalCypher.js
@@ -2,7 +2,12 @@ function encode(str,  n)
 {
   //letter to numeric value
 
-
+  let nArray = n.toString().split('')
+  let nCounter = 0
+  nArray = nArray.map(element =>{
+   return Number(element)
+  })
+  
   
   let strToArr = str.split('')
   
@@ -14,5 +19,16 @@ function encode(str,  n)
 return letter.charCodeAt(0) - numero
 
   })
-  console.log(answer)
+  
+  for(let i = 0; i< answer.length; i++){
+   if( nCounter < nArray.length) {
+     answer[i]+=nArray[nCounter]
+     nCounter++
+     }else{
+       nCounter = 0
+       answer[i]+=nArray[nCounter]
+     nCounter++
+     }
+  }
+  return answer
 }
