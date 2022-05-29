06992f7327b766b7c616cb057467990048cb2170
Connor <connor.doherty97@gmail.com>
2022-05-29 14:19:25 -0400

    pseudo

--- countTheDivisibleNumbers.js
+++ countTheDivisibleNumbers.js
@@ -1,4 +1,13 @@
 function divisibleCount(x, y, k) {
     //code me
+    function divisibleCount(x, y, k) {
+        //code me
+       
+       // return array of nums from x to y
+       
+       //if (arr[i]%k === 0) {counter++}
+       
+     }
+     
  }
  
