316b21d8b477859a31ba6284b5785d78d7cb9668
Connor <connor.doherty97@gmail.com>
2022-05-30 19:33:14 -0400

    pseudo

--- theDeafRatsOfHamelin.js
+++ theDeafRatsOfHamelin.js
@@ -1,5 +1,11 @@
 var countDeafRats = function(town) {
     // Your code here
+    
+    //split into an array, then test to see if each tilde on either side of the P is going in the right decision, keep track w/counter
+    
+    let badRats = 0
+    
+    
     return 0;
   }
   
