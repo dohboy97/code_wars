ed2e844982d34d133bd82ad7e8485dd917da69b3
Connor <connor.doherty97@gmail.com>
2022-06-01 16:20:34 -0400

    pseudo

---
+++ buildTower.js
@@ -0,0 +1,12 @@
+function towerBuilder(nFloors) {
+    // build here
+    
+    let tower = []
+    //array to contain the tower
+    
+    let floor = 1
+    //number of asteriks, add two each time in for loop
+    
+    //for loop will add the floors - need to also account for the spaces though
+    
+  }
