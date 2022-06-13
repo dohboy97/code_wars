1386ff1581d448e4dbd58499b6c5ffc000c2a7e1
Connor <connor.doherty97@gmail.com>
2022-06-12 21:40:15 -0400

    vars figured

---
+++ buyingACar.js
@@ -0,0 +1,12 @@
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+    //your code here
+    
+    let oldCarVal = startPriceOld
+    let newCarVal = startPriceNew
+    let percentLoss = percentLossByMonth
+    let monthCounter = 0
+    let savings = 0
+    // loop for while savings < price of new car
+    
+    
+  }
