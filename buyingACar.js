90ca6832ef28d27dff1de1fe3a178d41ae4beca5
Connor <connor.doherty97@gmail.com>
2022-06-12 21:52:16 -0400

    attempt 1

--- buyingACar.js
+++ buyingACar.js
@@ -6,7 +6,19 @@ function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMon
     let percentLoss = percentLossByMonth
     let monthCounter = 0
     let savings = 0
+    let arr = [0,0]
     // loop for while savings < price of new car
     
-    
+    while(savings < newCarVal - oldCarVal){
+      if(monthCounter % 2 === 0){
+        percentLoss += .5
+      }
+      savings+= savingperMonth
+      oldCarVal = oldCarVal - (oldCarVal * percentLoss)
+      newCarVal = newCarVal - (newCarVal * percentLoss)
+      monthCounter++
+      arr[0]=monthCounter
+        arr[1]= savings - (newCarVal - oldCarVal)
+    }
+    return arr
   }
