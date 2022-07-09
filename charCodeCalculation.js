5738fdc38b7b96cf44afc925cb93ebdcd36c2045
Connor <connor.doherty97@gmail.com>
2022-07-09 14:03:57 -0400

    pseudo

---
+++ charCodeCalculation.js
@@ -0,0 +1,17 @@
+function calc(x){
+
+    // use .charCodeAt()
+    
+    let total1 = ''
+    
+    let total2 = ''
+    
+    //use a for loop that adds tje ascii codes to total 1
+    
+    //then turn the string into an array
+    
+    //then run a for loop replacing each incidence of 7 in the array with 1 in a second variable, total2
+    
+    //then return the difference between the sum of their digits
+    
+  }
